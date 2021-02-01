03148eccb34303d567a135a6c8a0e9f21db09043
karhis <otsopa@gmail.com>
2021-02-01 16:28:48 +0200

    flask ja postresql setup

---
+++ venv/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    result = db.session.execute("SELECT COUNT(*) FROM messages")
+    count = result.fetchone()[0]
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    result = db.session.execute("SELECT title FROM messages")
+    titles= result.fetchall()
+    return render_template("index.html", count=count, messages=messages, titles = titles)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)"
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
