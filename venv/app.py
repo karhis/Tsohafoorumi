302693e2943bf42c8a13d7defe158181439e44ba
karhis <otsopa@gmail.com>
2021-02-04 17:43:28 +0200

    barebones postaus

--- venv/app.py
+++ venv/app.py
@@ -1,32 +1,70 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    result = db.session.execute("SELECT COUNT(*) FROM messages")
-    count = result.fetchone()[0]
-    result = db.session.execute("SELECT content FROM messages")
-    messages = result.fetchall()
-    result = db.session.execute("SELECT title FROM messages")
-    titles= result.fetchall()
-    return render_template("index.html", count=count, messages=messages, titles = titles)
+    sql = "SELECT id, title, created_by, thanks, date FROM threads ORDER BY id DESC"
+    result = db.session.execute(sql)
+    titles = result.fetchall()
+    sql = "SELECT COUNT(*) FROM threads"
+    result = db.session.execute(sql)
+    amount = result.fetchone()[0]
+    return render_template("index.html", titles=titles, amount=amount)
 
 @app.route("/new")
 def new():
     return render_template("new.html")
 
-
-@app.route("/send", methods=["POST"])
-def send():
-    content = request.form["content"]
-    sql = "INSERT INTO messages (content) VALUES (:content)"
-    db.session.execute(sql, {"content":content})
+@app.route("/reply", methods=["POST"])
+def reply():
+    thread_id = request.form["id"]
+    message = request.form["message"]
+    sql = "INSERT INTO messages (content, thread_id, created_by, thanks, date) VALUES (:message, :thread_id, 69, 0, NOW())"
+    db.session.execute(sql, {"message":message, "thread_id":thread_id})
     db.session.commit()
+    return redirect("/thread/"+str(thread_id))
+
+
+@app.route("/create", methods=["POST"])
+def send():
+    title = request.form["title"]
+    sql = "INSERT INTO threads (title, created_by, thanks, date) VALUES (:title, 69, 0, NOW()) RETURNING id"
+    result = db.session.execute(sql, {"title":title})
+    thread_id = result.fetchone()[0]
+    message = request.form["message"]
+    sql = "INSERT INTO messages (content, thread_id, created_by, thanks, date) VALUES (:message, :thread_id, 69, 0, NOW())"
+    db.session.execute(sql, {"message":message, "thread_id":thread_id})
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/thread/<int:id>")
+def thread(id):
+    sql = "SELECT title FROM threads WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    title = result.fetchone()[0]
+    sql = "SELECT content, created_by, thanks, date FROM messages WHERE thread_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    messages = result.fetchall()
+    return render_template("thread.html", title=title, messages=messages, id=id)
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
     return redirect("/")
