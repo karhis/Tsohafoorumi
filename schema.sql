67a91b65611f0da9aa33792df6c0854bb651afe4
Otso P <59831776+karhis@users.noreply.github.com>
2021-02-05 20:14:09 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE threads (
     id SERIAL PRIMARY KEY,
     title TEXT,
-    created_by INTEGER,
+    created_by TEXT,
     date TIMESTAMP
 );
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     thread_id INTEGER,
-    created_by INTEGER,
+    created_by TEXT,
     date TIMESTAMP
 );
 CREATE TABLE users (
@@ -22,5 +22,5 @@ CREATE TABLE thanks (
     id SERIAL PRIMARY KEY,
     thread_id INTEGER,
     message_id INTEGER,
-    created_by INTEGER
+    created_by TEXT
 );
