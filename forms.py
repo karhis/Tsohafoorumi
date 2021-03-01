2ccf1d830c8a9c8e11a2c8d1e95034f1231f9317
karhis <otsopa@gmail.com>
2021-03-01 12:53:37 +0200

    Edellisestä commitista unohtuneet

---
+++ forms.py
@@ -0,0 +1,6 @@
+from wtforms import Form, StringField, PasswordField, validators
+
+class RegistartionForm(Form):
+    username = StringField('Käyttäjätunnus', [validators.InputRequired(), validators.Length(min=1, max=32)])
+    password = PasswordField('Salasana', [validators.InputRequired(), validators.EqualTo('confirm', message='Salasanat eivät täsmää')])
+    confirm = PasswordField('Vahvista salasana')
